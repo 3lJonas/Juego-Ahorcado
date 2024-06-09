c6dbfb3c600328225afe73696ab6f667a3f16a3d
3lJonas <jlozada4554@uta.edu.ec>
2024-06-08 22:24:32 -0500

    Se hizo modificaciones a la logica de negocio en torneo y ronda

--- JuegoAhorcadoConsola/src/juegoahorcadoconsola/Ronda.java
+++ JuegoAhorcadoConsola/src/juegoahorcadoconsola/Ronda.java
@@ -7,39 +7,30 @@ package juegoahorcadoconsola;
 
 import java.util.HashSet;
 import java.util.Set;
+import vista.Interfaz_Juego;
 
 /**
  *
  * @author ASUS VIVOBOOK
  */
 public class Ronda {
+
     Palabra palabra;
     private StringBuilder palabraOculta;
     private Set<Character> letrasUsadas;
-    private int intentosRestantes;
-    private String[] ahorcado;
+    private Interfaz_Juego intJuego;
 
-    public Ronda(Palabra palabra) {
+    public Ronda(Palabra palabra, Interfaz_Juego intJuego) {
         this.palabra = palabra;
         this.palabraOculta = new StringBuilder();
         for (int i = 0; i < palabra.getPalabra().length(); i++) {
             this.palabraOculta.append("*");
         }
         this.letrasUsadas = new HashSet<>();
-        this.intentosRestantes = 6;
-        this.ahorcado = new String[]{
-            "+---+\n    |\n    |\n    |\n    |\n    |\n=========",
-            "+---+\n    |\n    |\n    |\n    |\n    |\n====o====",
-            "+---+\n    |\n    |\n    |\n    |\n    |\n======o===",
-            "+---+\n    |\n    |\n    |\n    |\n    |\n=====o====",
-            "+---+\n    |\n    |\n    |\n    |\n    |\n=====o====\n    /     ",
-            "+---+\n    |\n    |\n    |\n    |\n    |\n=====o====\n    /\\    ",
-            "+---+\n    |\n    |\n    |\n    |\n    |\n=====o====\n    /\\    \n   /      ",
-            "+---+\n    |\n    |\n    |\n    |\n    |\n=====o====\n    /\\    \n   / \\    "
-        };
+        this.intJuego = intJuego;
     }
 
-    public boolean adivinarLetra(char letra) {
+    public boolean adivinarLetra(char letra, Jugador jugador) {
         if (letrasUsadas.contains(letra)) {
             return false;
         }
@@ -55,7 +46,7 @@ public class Ronda {
         }
 
         if (!adivinado) {
-            intentosRestantes--;
+            jugador.setIntentos(jugador.getIntentos() - 1);
         }
 
         return adivinado;
@@ -65,20 +56,31 @@ public class Ronda {
         return !palabraOculta.toString().contains("*");
     }
 
-    public int getIntentosRestantes() {
-        return intentosRestantes;
-    }
-
     public void mostrarEstado(Jugador jugador) {
-        System.out.print("\nPalabra: " + palabraOculta + "\nIntentos restantes: " + intentosRestantes + "\n" + ahorcado[6 - intentosRestantes] + "\n\n");
-        System.out.print(jugador.getNombre() + ", adivina una letra: ");
+        switch (jugador.getNombre()) {
+            case "Jugador 1":
+                intJuego.palabraJugador1.setText(palabraOculta.toString());
+                break;
+            case "Jugador 2":
+                intJuego.palabraJugador2.setText(palabraOculta.toString());
+                break;
+            case "Jugador 3":
+                intJuego.palabraJugador3.setText(palabraOculta.toString());
+                break;
+            case "Jugador 4":
+                intJuego.palabraJugador4.setText(palabraOculta.toString());
+                break;
+        }
+        intJuego.jLabelIntentos.setText("Intentos: " + jugador.getIntentos());
     }
 
-    public boolean juegoTerminado() {
-        return intentosRestantes == 0 || palabraAdivinada();
+    public boolean juegoTerminado(Jugador jugador) {
+        return jugador.getIntentos() == 0 || palabraAdivinada();
     }
 
-    public int obtenerPuntaje() {
-        return intentosRestantes;
+    public int obtenerPuntaje(Jugador jugador) {
+        int puntos = palabraAdivinada() ? 120 : 0;
+        jugador.sumarPuntaje(puntos);
+        return jugador.getPuntaje();
     }
 }
